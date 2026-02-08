fc3d51b3673e1fa87b4bbedcedbcbe67197bad17
merkuloko <mollucrem@gmail.com>
2026-02-09 00:36:43 +0800

    Add index.py to the api directory

--- NGLNiGM/api/index.py
+++ NGLNiGM/api/index.py
@@ -2,8 +2,8 @@ import os
 from flask import Flask, render_template, request, jsonify, session, redirect
 from supabase import create_client, Client
 
-# Vercel looks for 'app', but 'main' works if configured in vercel.json
-app = Flask(__name__)
+# Tell Flask the templates are in the root templates folder
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.secret_key = os.environ.get('SECRET_KEY', 'pogi_si_gm_default')
 
 # Pulling keys from Vercel Environment Variables
